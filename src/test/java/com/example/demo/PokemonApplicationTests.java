16c78945c73cb24ff2518165e5b4d3c219852911
paweu9000 <wolfenorrin5@gmail.com>
2022-08-30 19:09:54 +0200

    database properties

--- src/test/java/com/example/demo/PokemonApplicationTests.java
+++ src/test/java/com/example/demo/PokemonApplicationTests.java
@@ -1,13 +1,25 @@
 package com.example.demo;
 
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.junit.Assert.*;
 
 @SpringBootTest
 class PokemonApplicationTests {
 
+	@Mock
+	MockMvc mockMvc;
+
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	public void mockNotNullTest() {
+		Assert.assertNotNull(mockMvc);
 	}
 
 }
